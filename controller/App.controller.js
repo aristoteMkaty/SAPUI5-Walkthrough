f988520eda4eb97bd1339b606ca68c479f40a6db
Katy Aristote <stotestorch@gmail.com>
2023-02-08 09:41:22 +0200

    First commit

---
+++ controller/App.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onShowHello : function () {
+           // show a native JavaScript alert
+           MessageToast.show("Hello World");
+        }
+     });
+});
